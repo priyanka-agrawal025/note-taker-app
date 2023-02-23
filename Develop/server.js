8fc28f9075b31d1ce3905f6647ed692d4e688871
Priyanka Agrawal <priyankaagrawal@Priyankas-MacBook-Pro.local>
2023-02-22 19:12:35 -0500

    new port again

--- Develop/server.js
+++ Develop/server.js
@@ -8,7 +8,7 @@ const app = express();
 const adapter = new fileSync('./db/db.json');
 const db = lowdb(adapter);
 
-const PORT = process.env.PORT;
+const PORT = 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
